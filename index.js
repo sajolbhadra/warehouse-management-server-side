d02d1082bf8d5cba7f0e99c49862021dc06e57fe
sajolbhadra <sajolbhadra@gmail.com>
2022-05-04 17:07:04 +0600

    node server initiated

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 require('dotenv').config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 //middleware
 app.use(cors())
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.60qwo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 app.get('/', (req, res) => {
     res.send('Server Online');
 })
 
-app.listen( port , () => {
+app.listen(port, () => {
     console.log('Server on port', port)
 })
