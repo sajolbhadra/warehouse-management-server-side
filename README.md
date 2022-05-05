ccd78c94da97dec82d2a16b7ce5f7914af3110ae
sajolbhadra <sajolbhadra@gmail.com>
2022-05-05 19:42:20 +0600

    readme updated

---
+++ README.md
@@ -0,0 +1,12 @@
+# Electronic-pulse [WareHouse](www.electronic-pulse.web.app) 
+
+## Features
+* The site featuring Firebase Authentication. User can sign in and 
+*  
+.
+
+## Used Technology
+* React Bootstrap
+* React Router Dom
+* Environmente Variable
+* Firebase Authentication
--- index.js
+++ index.js
@@ -13,6 +13,25 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run() {
+    try{
+        await client.connect();
+        console.log('server online')
+        const electronicsCollection = client.db('electronics').collection('items'); 
+        const usersCollection = client.db('electronics').collection('users'); 
+
+        
+
+
+
+    }
+    finally{
+        // await client.close();
+    }
+}
+
+run().catch(console.dir)
+
 app.get('/', (req, res) => {
     res.send('Server Online');
 })
